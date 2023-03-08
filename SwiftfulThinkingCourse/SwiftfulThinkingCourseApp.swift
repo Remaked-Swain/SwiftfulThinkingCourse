11072e4005fc3e34f90960be6595b0cccd12f569
Remaked-Swain <destap@naver.com>
2023-03-08 22:44:43 +0900

    creation

---
+++ SwiftfulThinkingCourse/SwiftfulThinkingCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingCourseApp.swift
+//  SwiftfulThinkingCourse
+//
+//  Created by Swain Yun on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
